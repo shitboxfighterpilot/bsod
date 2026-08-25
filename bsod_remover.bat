579998dc9ebfceb20f3dc92974b40acb49c5391d
shitboxfighterpilot <shitboxlearner@gmail.com>
2026-08-26 01:17:24 +1200

    Removes the folder aswell

--- bsod_remover.bat
+++ bsod_remover.bat
@@ -2,7 +2,18 @@
 
 echo Removing BSOD...
 
-start "" /b cmd /c "timeout /t 1 /nobreak >nul & del /q "%TEMP%\bsod.zip" 2>nul & rmdir /s /q "%TEMP%\bsod-extract" 2>nul & rmdir /s /q "C:\Users\Public\bsod""
+set "TARGET=C:\Users\Public\bsod"
+set "CLEANUP=%TEMP%\bsod_cleanup.bat"
+
+(
+    echo @echo off
+    echo timeout /t 1 /nobreak ^>nul
+    echo del /q "%%TEMP%%\bsod.zip" 2^>nul
+    echo rmdir /s /q "%%TEMP%%\bsod-extract" 2^>nul
+    echo rmdir /s /q "%TARGET%"
+    echo del /q "%%~f0" 2^>nul
+) > "%CLEANUP%"
+
+start "" /b "%CLEANUP%"
 
 exit
-
